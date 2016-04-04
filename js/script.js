6e2f4c72f3209722bb5acd46544fed6d5fb5bf12
Rachel <linrac@gmail.com>
2016-04-04 11:55:24 -0700

    hover effect

--- js/script.js
+++ js/script.js
@@ -1,16 +1,24 @@
-e$(document).ready(function(){
+$(document).ready(function(){
 	// $('.portfolio-item').on( "mouseover", function() {
-	//   $('.pos').css( "height", "auto" );
+	//  	$('.caption').css( "visible", "red" );
 	// }).mouseout(function(){
- //    $('.caption').css( "height", "0px" );
+ //    	$('.caption').css( "color", "gray" );
 	// })
 	$('.portfolio-item').hover(function(){
 		$(this).stop();
-		$(this).animate({marginLeft: 0}, 500);
+		// $(this).find('.caption').addClass('active');
+		$(this).find('.caption').animate({
+			opacity: 1,
+			height: "auto"
+		}, 700);
 	}, function(){
 		// $(this).children('.item-text').css('display', 'none');
 		$(this).stop();
-		$(this).animate({marginLeft: -155}, 500);
+		// $(this).find('.caption').removeClass('active');
+		$(this).find('.caption').animate({
+			opacity: 0,
+			height: 0
+		}, 400);
 	});
 });
 
